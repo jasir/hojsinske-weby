08a0101a62e0abb2f8474885bb65fb667d0c3b4c
Jaroslav Povolný <jaroslav.povolny@gmail.com>
2025-07-01 21:32:42 +0200

    layouts working

--- app/Presentation/hojsin.cz/Page/PagePresenter.php
+++ app/Presentation/hojsin.cz/Page/PagePresenter.php
@@ -10,9 +10,9 @@ final class PagePresenter extends Nette\Application\UI\Presenter
 {
     public function renderDefault(string $page = 'default'): void
     {
-
         $templateFile = __DIR__ . "/templates/$page.latte";
         if (file_exists($templateFile)) {
+            $this->setLayout(__DIR__ . '/../@layout.latte');
             $this->template->setFile($templateFile);
         } else {
             $this->error('Stránka nebyla nalezena');
--- app/Presentation/penzionsborovna.cz/Page/PagePresenter.php
+++ app/Presentation/penzionsborovna.cz/Page/PagePresenter.php
@@ -10,9 +10,10 @@ final class PagePresenter extends Nette\Application\UI\Presenter
 {
     public function renderDefault(string $page = 'default'): void
     {
-
         $templateFile = __DIR__ . "/templates/$page.latte";
         if (file_exists($templateFile)) {
+            // Explicitně nastavíme layout PŘED setFile()
+            $this->setLayout(__DIR__ . '/../@layout.latte');
             $this->template->setFile($templateFile);
         } else {
             $this->error('Stránka nebyla nalezena');
