25a27198c02f16e518bf4fa1cb6613a270e4eb0c
Jaroslav Povolný <jaroslav.povolny@gmail.com>
2025-07-01 19:00:14 +0200

    initial commit

---
+++ app/Presentation/Page/PagePresenter.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presentation\Page;
+
+use Nette;
+
+final class PagePresenter extends Nette\Application\UI\Presenter
+{
+    public function renderDefault(string $page = 'default'): void
+    {
+        $templateFile = __DIR__ . "/templates/$page.latte";
+        if (file_exists($templateFile)) {
+            $this->template->setFile($templateFile);
+        } else {
+            $this->error('Stránka nebyla nalezena');
+        }
+    }
+}
---
+++ www/assets/script.js
@@ -0,0 +1,1 @@
+console.log("Hojsin Web loaded.");
