fd17a400254ba590c0ca07a979bd97b961095c5d
Jaroslav Povolný <jaroslav.povolny@gmail.com>
2025-07-13 13:51:59 +0200

    WIP

--- assets/hojsin.cz/default.js
+++ assets/hojsin.cz/default.js
@@ -1,3 +1,5 @@
 import './css/styles.pcss';
 import './scripts/hojsin-scripts';
 import './scripts/map.js';
+
+console.log('hello world !!!');
--- www/assets/hojsin.cz/script.js
+++ www/assets/hojsin.cz/script.js
@@ -114,6 +114,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+console.log('hello world !!!');
 
 /***/ }),
 
